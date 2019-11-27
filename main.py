c46ac29cc9520e5b40bf894b5bf1125c31bbe7bc
Leirda01 <adrieldj@orange.fr>
2019-11-27 15:48:52 +0100

    Importe plus de méthodes de PyQt5

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from PyQt5.QtWidgets import QApplication, QLabel
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+
 with open ("ipsum", "r") as fichier:
-    pass
-app = QApplication([])
-label = QLabel("Hello world!")
-label.show()
-app.exec_()
+    app = QApplication([])
+    label = QLabel(fichier.read())
+    label.show()
+    app.exec_()
