23c9fe27bc5bffaed892debc36533cd0db0dcd4d
Leirda01 <adrieldj@orange.fr>
2019-11-28 16:01:52 +0100

    renomme boutons1 et 1 et boutons _fayal et _adriel

--- main.py
+++ main.py
@@ -4,26 +4,16 @@ from PyQt5.QtCore import *
 from PyQt5.QtWidgets import QApplication, QPushButton
 import sys 
 
-app = QApplication.instance() 
-if not app: 
-    app = QApplication(sys.argv)
-fen = QWidget ()
+app = QApplication([]) 
+fen = QWidget()
 
-bouton1= QPushButton("Fayal")
-bouton2= QPushButton("Adriel")
+bouton_fayal = QPushButton("Fayal")
+bouton_adriel = QPushButton("Adriel")
 
 layout = QVBoxLayout()
-layout.addWidget(bouton1)
-layout.addWidget(bouton2)
+layout.addWidget(bouton_fayal)
+layout.addWidget(bouton_adriel)
 fen.setLayout(layout)
 
 fen.show()
 app.exec_()
-
-    
-    
-    #with open ("ipsum", "r") as fichier:
-    #    app = QApplication([])
-    #   label = QLabel(fichier.read())
-    #  label.show()
-    # app.exec_()
